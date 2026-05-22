1e662fc5c03f2d118767e7f2b2a7b8266946bbc9
HanYeWan <3296354387@qq.com>
2026-05-22 10:42:19 +0800

    feat: 完成登录、验证码功能与后端链接

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const request = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 请求拦截
+request.interceptors.request.use((config) => {
+  return config;
+});
+
+// 响应拦截
+request.interceptors.response.use(
+  (res) => res.data,
+  (err) => {
+    const msg = err.response?.data?.detail || '请求失败';
+    alert(msg);
+    return Promise.reject(err);
+  }
+);
+
+export default request;
